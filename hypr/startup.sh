7c0da8e72399121e84cd181bb32c41119aba4e2a
FabianTsch <tschohl.fabian@gmail.com>
2022-09-28 16:43:28 +0200

    add Hypr/Hyprland to repo

---
+++ hypr/startup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+picom -b
+nitrogen --restore
+batsignal -w 25 -c 10 -d 5 &
+xset s 350 120
+xss-lock --transfer-sleep-lock -n 'bash /usr/share/doc/xss-lock/dim-screen.sh' -- betterlockscreen --off 15 -l blur &
+/usr/bin/gnome-keyring-daemon --start --components=ssh,secrets,pkcs11 &
+clight &
+clipmenud &
+polkit-dumb-agent &
