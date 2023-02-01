9170f3a65a6b64f9ae55ee3ae388f968823c495f
FabianTsch <tschohl.fabian@gmail.com>
2023-02-01 10:36:23 +0100

    extended timeout in swayidle

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -43,9 +43,9 @@ pgrep gnome-keyring-d > /dev/null ||
 #    kdeconnect-indicator &
 
 pgrep swayidle > /dev/null || 
-    swayidle -w timeout 120 'hyprctl dispatch dpms off' \
+    swayidle -w timeout 180 'hyprctl dispatch dpms off' \
                 resume 'hyprctl dispatch dpms on' \
-                timeout 180 'hyprctl dispatch dpms on && /home/fabian/.config/hypr/scripts/lock & sleep 2 && hyprctl dispatch dpms off' \
+                timeout 240 'hyprctl dispatch dpms on && /home/fabian/.config/hypr/scripts/lock & sleep 2 && hyprctl dispatch dpms off' \
                 resume 'hyprctl dispatch dpms on' \
                 timeout 10 'if pgrep -x swaylock; then hyprctl dispatch dpms off; fi' \
                 resume 'hyprctl dispatch dpms on' &
