2bb3f566a8e1d7f6436108e49323158094e991ed
FabianTsch <tschohl.fabian@gmail.com>
2023-06-16 11:17:02 +0200

    add lid switch support

---
+++ hypr/scripts/switch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+if grep open /proc/acpi/button/lid/LID0/state; then
+    hyprctl keyword monitor "eDP-1,preferred,auto,1"
+else
+    if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
+        hyprctl keyword monitor "eDP-1, disable"
+    fi
+fi
