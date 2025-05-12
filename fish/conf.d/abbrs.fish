2099f85d28ceb2c9eab1e279df432550634903a4
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2025-05-12 17:05:30 +0200

    feat(fish): provide git `Co-author` abbreviation

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -1,5 +1,6 @@
 abbr --add gs 'git status' 
 abbr --add gsh 'git status --untracked-files=no'
 abbr --add gl 'git log --graph --pretty=format:"%C(yellow)%h%Creset%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
+abbr --add gca 'git shortlog -sne | fzf --multi | cut -f2 -d (printf "\t") | sed \'s/^/Co-authored-by:\ /\''
 
 abbr --add lf 'lfcd'
