6f47b943318fb06dc11e32bbc2d003d2bd706db2
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2025-05-13 12:16:35 +0200

    feat(fish): provide hyprctl abbreviation for valhalla

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -4,3 +4,12 @@ abbr --add gl 'git log --graph --pretty=format:"%C(yellow)%h%Creset%C(auto)%d%Cr
 abbr --add gca 'git shortlog -sne | fzf --multi | cut -f2 -d (printf "\t") | sed \'s/^/Co-authored-by:\ /\''
 
 abbr --add lf 'lfcd'
+
+abbr --add valhalla 'killall waybar && hyprctl keyword monitor DP-5,preferred,auto,2.0 && hyprctl keyword monitor DP-7,preferred,auto,2.0 && waybar > /dev/null 2>&1 & disown'
+
+if type -q fdfind; and not type -q fd
+    abbr --add fd 'fdfind'
+end
+if type -q batcat; and not type -q bat
+    abbr --add bat 'batcat'
+end
