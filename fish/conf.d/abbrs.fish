a5112d6b40dc296cb2867c28d8f4557412b63f9c
Fabian Tschohl <fabian.tschohl@omicronenergy.com>
2025-08-08 13:53:41 +0200

    feat(fish): print `git status` in `--short` format

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -1,5 +1,5 @@
-abbr --add gs 'git status' 
-abbr --add gsh 'git status --untracked-files=no'
+abbr --add gs 'git status --short'
+abbr --add gsh 'git status --untracked-files=no --short'
 abbr --add gl 'git log --graph --pretty=format:"%C(yellow)%h%Creset%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
 abbr --add gca 'git shortlog -sne | fzf --multi | cut -f2 -d (printf "\t") | sed \'s/^/Co-authored-by:\ /\''
 
